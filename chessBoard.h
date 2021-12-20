3f0ba14e3d8e37b424287cdf1978ae0ca966777e
Luca Fantin <ferrarif1w@gmail.com>
2021-12-20 22:04:28 +0100

    Co-authored-by: G4II0 <G4II0@users.noreply.github.com>

--- chessBoard.h
+++ chessBoard.h
@@ -1,11 +1,20 @@
 #include <vector>
+#include <string>
 using namespace std;
 
 class ChessBoard {
     private:
         vector<vector<Pieces*>> board;
+        const int SIZE = 8;
+        bool checkBoundaries(pair<int, int> pos);
     public:
+        struct Move {
+            Pieces* piece;
+            pair<int, int> destination;
+            string moveName = 0;
+        };
         ChessBoard();
-        
-}
-//sucasuca
+        vector<Move*> movesAvailable();
+        void performMove(int moveIndex); //indice da vector<Move*> precedente
+        string printBoard();
+};
