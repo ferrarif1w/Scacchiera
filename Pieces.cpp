4db1db1e95678bf174339197ec3efc67e1a75f7f
G4II0 <smugallo@gmail.com>
2021-12-27 19:38:06 +0100

    m

--- Pieces.cpp
+++ Pieces.cpp
@@ -2,4 +2,196 @@
 #include <vector>
 #include "Pieces.h"
 
-using namespace std;
+//Alfiere
+Pieces::A() {}
+
+Pieces::A(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+void Pieces::SetColor(char Col)
+{
+    Color = Col;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+//Cavallo
+Pieces::C() {}
+
+Pieces::C(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+void Pieces::SetColor(char Col)
+{
+    Color = Col;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+//Donna
+Pieces::D() {}
+
+Pieces::D(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+void Pieces::SetStatus(bool Mov)
+{
+    Moved = Mov;
+}
+
+bool Pieces::GetStatus()
+{
+    return Moved;
+}
+
+//Pedone
+Pieces::P() {}
+
+Pieces::P(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+void Pieces::SetStatus(bool Mov)
+{
+    Moved = Mov;
+}
+
+bool Pieces::GetStatus()
+{
+    return Moved;
+}
+
+//Re
+Pieces::R() {}
+
+Pieces::R(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+void Pieces::SetStatus(bool Mov)
+{
+    Moved = Mov;
+}
+
+bool Pieces::GetStatus()
+{
+    return Moved;
+}
+
+//Torre
+Pieces::T() {}
+
+Pieces::T(pair<int, int> pos, char col)
+{
+    pair<int, int> position = pos;
+    char Color = col;
+}
+
+void Pieces::SetMove(pair<int, int> NewPosition)
+{
+    position = NewPosition;
+}
+
+pair<int, int> Pieces::GetPosition()
+{
+    return position;
+}
+
+char Pieces::GetColor()
+{
+    return Color;
+}
+
+void Pieces::SetStatus(bool Mov)
+{
+    Moved = Mov;
+}
+
+bool Pieces::GetStatus()
+{
+    return Moved;
+}
--- Pieces.h
+++ Pieces.h
@@ -2,15 +2,30 @@
 #define PIECES_H
 
 #include <vector>
-using namespace std;
 
 class Pieces
 {
 
+        protected:
+
+        std::pair<int, int> position;
+        char Color;
+        bool Moved;
+
+
     public:
 
         Pieces();
-        Pieces(pair<int, int> position, char color);
+        Pieces(std::pair<int, int> position, char color);
 
+        void SetMove(std::pair<int, int> NewPosition);
+        std::pair<int, int> GetPosition();
+
+        char GetColor();
+
+        void SetStatus(bool Mov);
+        bool GetStatus();
+
+        std::vector<std::pair<int, int>*> Pmove();
 };
 #endif
