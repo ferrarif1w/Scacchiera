8e9cb02f0550c748e98e6ba423df5e347490942c
G4II0 <smugallo@gmail.com>
2022-01-05 21:47:26 +0100

    bau

---
+++ Bot.h
@@ -0,0 +1,19 @@
+//Gallo Samuele 200155/104
+
+#include <iostream>
+#include "chessBoard.h"
+#include "Pieces.h"
+#include <algorithm>
+
+class Gallo //sio porco
+{
+    protected: //magari lo fosse stato il mio ano
+
+
+
+
+    public: //come il mio ano
+
+
+
+};
